88af52a12da0bdcaf30ae0a7d8f942c8947d2d88
joabpereira <joabpereira356@gmail.com>
2025-12-05 09:37:41 -0300

    feat: Componente product-service criado e carregando produtos via api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { Card1MainComponent } from './componentes/card1-main/card1-main.component';
 import { CardProdutosComponent } from './componentes/card-produtos/card-produtos.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- src/app/componentes/card-produtos/card-produtos.component.ts
+++ src/app/componentes/card-produtos/card-produtos.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product-service/product.service';
+import { CurrencyPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-card-produtos',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CurrencyPipe],
   templateUrl: './card-produtos.component.html',
-  styleUrl: './card-produtos.component.scss',
+  styleUrls: ['./card-produtos.component.scss'],
 })
-export class CardProdutosComponent {
+export class CardProdutosComponent implements OnInit {
   produtoImg = 'imagens/camisa.jpg';
   imgHeight = 300;
+
+  constructor(private productService: ProductService) {}
+
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.productService.getProdutos().subscribe({
+      next: (data) => (this.products = data),
+      error: (err) => console.error('Erro ao carregar produtos', err),
+    });
+  }
 }
